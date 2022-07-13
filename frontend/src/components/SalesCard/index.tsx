358ff8a2d430253a4347b4e99088a02db39ffdab
Rafael-Boscolo <rbf.boscolo@gmail.com>
2022-07-12 21:48:04 -0300

    Other components

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -66,20 +66,6 @@ function SalesCard() {
                             <td>
                                 <div className="dsmeta-red-btn-container">
                                     <NotificationButton />
-
-                                </div>
-                            </td>
-                        </tr><tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
-                            <td>R$ 55300.00</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-
                                 </div>
                             </td>
                         </tr>
